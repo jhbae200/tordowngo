024e5965bd0f16be763a53f00c44ed076e03de45
jinhwan <jhbae@anipen.com>
2017-04-25 18:03:17 +0900

    update

--- app/task/file.go
+++ app/task/file.go
@@ -10,6 +10,7 @@ import (
 	"github.com/anacrolix/torrent"
 	"github.com/revel/revel"
 	"golang.org/x/time/rate"
+	"tordowngo/app/database"
 )
 
 type File struct {
@@ -19,20 +20,28 @@ type File struct {
 
 func (j File) Run() {
 	revel.INFO.Println(j.File.FileName + "staring job..")
+	db := database.GetDb()
 	if j.File.FileType == "application/x-bittorrent" {
-		revel.INFO.Println(j.UserNum)
+		j.File.FileStatus = 9
+		db.Save(&j.File)
 		config := torrent.Config{}
 		config.DownloadRateLimiter = rate.NewLimiter(rate.Limit(3*1024*1024), 1<<20)
 		fileNames, err := utilTorrent.Down(j.File.FileName, config)
 		if err != nil {
 			revel.ERROR.Panic(err)
+			j.File.FileStatus = 8
+			db.Save(&j.File)
 		}
+		j.File.FileStatus = 7
+		db.Save(&j.File)
 		for _, fileName := range fileNames {
 			if err := j.move(fileName); err != nil {
 				revel.ERROR.Panic(err)
 			}
 		}
 	} else {
+		j.File.FileStatus = 7
+		db.Save(&j.File)
 		if err := j.move(j.File.FileName); err != nil {
 			revel.ERROR.Panic(err)
 		}
